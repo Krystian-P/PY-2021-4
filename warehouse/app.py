b7fec234e1e92de3d1998047be6e955350f19b6a
Krystian <krystianbezpalko@gmail.com>
2021-06-09 20:22:16 +0200

    Aktualizacje

--- warehouse/app.py
+++ warehouse/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, redirect, app
+from flask import Flask, render_template, request, redirect
 
-main = Flask(__name__)
+app = Flask(__name__)
+
+
+@app.route('/')
+def title():
+    return f"Witaj"
 
 
 @app.route('/message', methods=['GET', 'POST'])
@@ -12,3 +17,16 @@ def message():
         print("We received POST")
         print(request.form)
         return redirect("/")
+
+
+@app.route("/warehouse", methods = ['GET'])
+def warehouse():
+    items = ["screwdriver", "hammer", "saw"]
+    error = ["hammer is broken!"]
+    return render_template("Warehouse_Child.html", items=items, errors = error)
+
+
+
+@app.route('/greeting')
+def greeting():
+    return render_template('greeting.html')
